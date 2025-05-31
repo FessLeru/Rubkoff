575d8b6eb5d1ae037622fe66e51fdd00dcad7c3f
FessLeru <iisusputmoj@gmail.com>
2025-05-31 20:04:50 +0300

    change dependence

--- setup.py
+++ setup.py
@@ -5,16 +5,16 @@ setup(
     version="1.0.0",
     packages=find_packages(),
     install_requires=[
-        "aiogram>=3.3.0",
-        "aiohttp>=3.9.0",
-        "aiosqlite>=0.19.0",
-        "beautifulsoup4>=4.12.0",
-        "httpx>=0.26.0",
-        "openai>=1.12.0",
-        "python-dotenv>=1.0.0",
-        "SQLAlchemy>=2.0.0",
-        "typing-extensions>=4.9.0",
-        "pydantic>=2.5.0",
-        "alembic>=1.13.0"
+        "aiogram==3.20.0.post0",
+        "aiohttp==3.11.18",
+        "aiosqlite==0.21.0",
+        "beautifulsoup4==4.13.4",
+        "httpx==0.28.1",
+        "openai==1.78.1",
+        "python-dotenv==1.1.0",
+        "SQLAlchemy==2.0.41",
+        "typing_extensions==4.13.2",
+        "pydantic==2.11.4",
+        "alembic==1.15.2"
     ]
 ) 
